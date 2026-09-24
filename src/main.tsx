7d01ba03f7b3c1dd21873f06607845d6b208fea5
Ashif Hassan <ashifhassan1996@gmail.com>
2026-09-24 11:18:00 +0530

    chore(frontend): update app entry and global styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import ConfirmModalProvider from "./components/common/confirm-modal/ConfirmModalProvider";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./store/store";
+import App from "./App";
+import GlowLoader from "@/components/PageLoader";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate
+        loading={
+          <div className="min-h-screen bg-[#0d1117] flex items-center justify-center">
+            <GlowLoader />
+          </div>
+        }
+        persistor={persistor}
+      >
+        <ConfirmModalProvider>
+          <App />
+        </ConfirmModalProvider>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
+);
